aa21827d6929a381f5b428afd004f48260728753
badat <you@example.com>
2020-02-19 11:28:07 +0700

    HinhChuNhat

---
+++ hinhchunhat.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHight;
+inputWidth = prompt('nhap chieu rong: ');
+inputHight = prompt('nhap chieu dai: ');
+let width = parseInt(inputWidth);
+let Hight = parseInt(inputHight);
+let s = width*Hight;
+document.write('s la: ' + s);
